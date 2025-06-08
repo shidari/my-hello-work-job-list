0e7cd9f1cd59564ec20ef74fb5aa7b27c636d38d
shidari <s.hidari@icloud.com>
2025-06-08 10:13:01 +0900

    スクレイピングのざっくりした基本設定

---
+++ packages/scraper/src/scraper.ts
@@ -0,0 +1,19 @@
+import { JSDOM } from "jsdom";
+import { type Result, err, ok } from "neverthrow";
+import type { ErrorMessage, Job } from "./type";
+
+export const scrapeJobDescription: (html: string) => Result<Job, ErrorMessage> =
+	(html: string) => {
+		const jsdom = new JSDOM(html);
+		const doc = jsdom.window.document;
+		const companyName = doc.querySelector("#ID_jgshMei")?.textContent;
+		const jobTitle = doc.querySelector("#ID_sksu")?.textContent;
+		const employmentType = doc.querySelector("#ID_kjKbn")?.textContent;
+		const workPlace = doc.querySelector("#ID_shgBsJusho")?.textContent;
+		if (!jobTitle) return err("not found jobTitle");
+		if (!companyName) return err("not found companyName");
+		if (!employmentType) return err("not found employmentType");
+		if (!workPlace) return err("not found workplace");
+
+		return ok({ companyName, jobTitle, employmentType, workPlace });
+	};
---
+++ packages/scraper/src/type.ts
@@ -0,0 +1,9 @@
+// APIエンドポイントとの齟齬なんとかしたいけど後回し
+export type Job = {
+	companyName: string;
+	jobTitle: string;
+	employmentType: string;
+	workPlace: string;
+};
+
+export type ErrorMessage = string;
