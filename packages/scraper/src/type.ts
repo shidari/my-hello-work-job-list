2c4b3c504588a49da2368391a68227f81841be0c
shidari <s.hidari@icloud.com>
2025-06-22 14:37:11 +0900

    cloud flare workersで計画してたスクレイピングを削除

--- packages/scraper/src/type.ts
+++
@@ -1,9 +0,0 @@
-// APIエンドポイントとの齟齬なんとかしたいけど後回し
-export type Job = {
-	companyName: string;
-	jobTitle: string;
-	employmentType: string;
-	workPlace: string;
-};
-
-export type ErrorMessage = string;
