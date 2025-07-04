3ce7bbf406700341f11d047a772f17c4e8857aef
shidari <s.hidari@icloud.com>
2025-07-04 18:52:28 +0900

    headless-crawler: Lambdaの呼び出しの監視機能の追加

--- packages/headless-crawler/lib/stack/headless-crawler-stack.ts
+++ packages/headless-crawler/lib/stack/headless-crawler-stack.ts
@@ -1,10 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import { Duration } from "aws-cdk-lib";
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
+import * as cloudwatch_actions from "aws-cdk-lib/aws-cloudwatch-actions";
 import * as events from "aws-cdk-lib/aws-events";
 import * as targets from "aws-cdk-lib/aws-events-targets";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import type { Construct } from "constructs";
 import * as dotenv from "dotenv";
@@ -75,6 +79,55 @@ export class HeadlessCrawlerStack extends cdk.Stack {
         JOB_STORE_ENDPOINT: process.env.JOB_STORE_ENDPOINT || "",
       },
     });
+
+    // SNSトピック
+    const crawlerAlarmTopic = new sns.Topic(this, "CrawlerAlarmTopic");
+    crawlerAlarmTopic.addSubscription(
+      new subs.EmailSubscription(process.env.MAIL_ADDRESS || ""),
+    );
+    const scraperAlarmTopic = new sns.Topic(this, "ScraperAlarmTopic");
+    scraperAlarmTopic.addSubscription(
+      new subs.EmailSubscription(process.env.MAIL_ADDRESS || ""),
+    );
+
+    // Lambda 実行回数のメトリクス
+    const crawlerInvocationsMetric = new cloudwatch.Metric({
+      namespace: "AWS/Lambda",
+      metricName: "Invocations",
+      statistic: "Sum",
+      period: cdk.Duration.hours(1),
+      dimensionsMap: {
+        FunctionName: crawler.functionName,
+      },
+    });
+    const scraperInvocationsMetric = new cloudwatch.Metric({
+      namespace: "AWS/Lambda",
+      metricName: "Invocations",
+      statistic: "Sum",
+      period: cdk.Duration.hours(1),
+      dimensionsMap: {
+        FunctionName: scraper.functionName,
+      },
+    });
+
+    const crawlingAlrm = new cloudwatch.Alarm(this, "CrawlerInvocationAlarm", {
+      metric: crawlerInvocationsMetric,
+      threshold: 1000,
+      evaluationPeriods: 1,
+      alarmDescription: "crawling Lambda invocation count exceeded threshold",
+    });
+    crawlingAlrm.addAlarmAction(
+      new cloudwatch_actions.SnsAction(crawlerAlarmTopic),
+    );
+    const scrapingAlarm = new cloudwatch.Alarm(this, "ScraperInvocationAlarm", {
+      metric: scraperInvocationsMetric,
+      threshold: 1000,
+      evaluationPeriods: 1,
+      alarmDescription: "scraping Lambda invocation count exceeded threshold",
+    });
+    scrapingAlarm.addAlarmAction(
+      new cloudwatch_actions.SnsAction(scraperAlarmTopic),
+    );
 
     scraper.addEventSource(
       new SqsEventSource(queue, {
