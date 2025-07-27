22406caa63117a4aedd02eb4f8a623427344f71e
shidari <s.hidari@icloud.com>
2025-07-27 22:37:49 +0900

    feat(schmea,scripts):schemaからdrizzleのスキーマを、job-storeにコピーするスクリプトの作成 - schemaの階層がおかしい(前にtypesも一緒に保存してた名残)が、後で直す - pre-commitで、実行するように - 事前ビルドがどこかで事故りそうなのでいつか別のものに変えたい

---
+++ packages/scripts/src/cli/copy-schema.mts
@@ -0,0 +1,13 @@
+import { argv } from "node:process"
+import { checkTargetPath, copyDrizzleSchema } from "../copy-schema.js"
+console.log('cwd:', process.cwd())
+const targetPath = argv[2]
+if (!targetPath) {
+    console.error('❌ Usage: command <target-path.ts>')
+    process.exit(1)
+}
+
+
+const result = await checkTargetPath(targetPath).asyncAndThen((targetPath) => copyDrizzleSchema(targetPath))
+
+result.match(() => console.log("schema copy succeeded!"), (e) => console.error(e))
---
+++ packages/scripts/src/copy-schema.ts
@@ -0,0 +1,74 @@
+import { execSync } from "node:child_process";
+import path from "node:path";
+import fs from "fs-extra";
+import {
+  type Result,
+  ResultAsync,
+  err,
+  errAsync,
+  ok,
+  okAsync,
+} from "neverthrow";
+
+const t = Symbol();
+type TargetTsFilePath = string & { [t]: unknown };
+export function targetPathArgsExists(relatizeTargetPath: string) {
+  if (!relatizeTargetPath) {
+    err({
+      type: "NotFounrTargetDirArg",
+      message: "Usage: ./copy-schema.mjs <target-directory>",
+    });
+  }
+  return ok(path.resolve(process.cwd(), relatizeTargetPath));
+}
+
+export function checkTargetPath(targetPath: string) {
+  if (targetPath.endsWith(".ts")) {
+    return ok(targetPath as TargetTsFilePath);
+  }
+  return err(
+    new Error(`❌ targetPath must end with ".ts": received "${targetPath}"`),
+  );
+}
+
+const r = Symbol();
+type RepoRootDirPath = string & { [r]: unknown };
+function findGitRootByGit(): Result<RepoRootDirPath, Error> {
+  try {
+    const root = execSync("git rev-parse --show-toplevel", {
+      encoding: "utf8",
+    }).trim();
+    return ok(root as RepoRootDirPath);
+  } catch (e) {
+    return err(new Error("Git root not found"));
+  }
+}
+
+const d = Symbol();
+type DrizzleSchemaPath = string & { [d]: unknown };
+function getDrizzleSchemaPath() {
+  return findGitRootByGit().asyncAndThen((repoRootDirPath) => {
+    const drizzleSchemaPath = path.resolve(
+      repoRootDirPath,
+      "./packages/schema/src/schema/job-store/drizzle.ts",
+    );
+    return ResultAsync.fromSafePromise(
+      fs.pathExists(drizzleSchemaPath),
+    ).andThen((b) => {
+      if (!b)
+        errAsync(
+          new Error(`drizzleSchemapath not found. path:${drizzleSchemaPath}`),
+        );
+      return okAsync(drizzleSchemaPath as DrizzleSchemaPath);
+    });
+  });
+}
+
+export function copyDrizzleSchema(targetPath: TargetTsFilePath) {
+  return getDrizzleSchemaPath().andThen((drizzleSchemaPath) => {
+    return ResultAsync.fromPromise(
+      fs.copy(drizzleSchemaPath, targetPath),
+      (e) => new Error(`❌ Failed to copy schema: ${String(e)}`),
+    );
+  });
+}
