ca76ee1cd99aaab37f7e54d993c5b1c4d9957dff
shidari <s.hidari@icloud.com>
2025-07-30 20:54:15 +0900

    chore(models/types): 不要なコードの削除

--- packages/models/src/types/job-store/jobInsert.ts
+++ packages/models/src/types/job-store/jobInsert.ts
@@ -1,18 +1,4 @@
 import type z from "zod";
-import type { insertJobRequestBodySchema, jobs } from "../../schemas";
-import type { KeysMustMatch } from "./helper";
-
-// ✅ エラーが出なければ一致
-type Check = KeysMustMatch<RawInsertBody, ZodRawInsertBody>;
-// drizzle の型推論（insert 型 or select 型）
-type JobDrizzle = typeof jobs.$inferInsert;
-type RawInsertBody = Omit<
-  JobDrizzle,
-  "id" | "createdAt" | "updatedAt" | "status"
->;
-// Zod の型
-type ZodRawInsertBody = InsertJobRequestBody;
-// 一旦キーだけ比較してる
-const check: Check = true;
+import type { insertJobRequestBodySchema } from "../../schemas";
 
 export type InsertJobRequestBody = z.infer<typeof insertJobRequestBodySchema>;
