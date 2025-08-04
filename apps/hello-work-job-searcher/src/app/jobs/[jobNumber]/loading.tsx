543a790b6e2fe5b8532b42d4d353e42d60ab5781
shidari <s.hidari@icloud.com>
2025-08-04 22:25:47 +0900

    feat(hello-work-job-searcher): 画面遷移時のローディングの追加

---
+++ apps/hello-work-job-searcher/src/app/jobs/[jobNumber]/loading.tsx
@@ -0,0 +1,6 @@
+import Loading from "../loading";
+
+export default function Page() {
+  // Or a custom loading skeleton component
+  return <Loading />;
+}
---
+++ apps/hello-work-job-searcher/src/app/jobs/loading.tsx
@@ -0,0 +1,6 @@
+import Loading from "../components/Loading";
+
+export default function Page() {
+  // Or a custom loading skeleton component
+  return <Loading />;
+}
